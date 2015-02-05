2f3ba773ea7d0db428edff0dcbf6fbf2647dd855
Nathan Burroughs <thenathanburroughs@gmail.com>
2015-02-05 08:49:09 +0000

    returning.py calculate AT content
    Now, edited so that it rounds to two digits after decimal point, and also calculates AT content of DNA sequence with lower case letters

--- returning.py
+++ returning.py
@@ -1,10 +1,10 @@
 from __future__ import division
 def get_at_content(dna):
     length = len(dna)
-    a_count = dna.count('A')
-    t_count = dna.count('T')
+    a_count = dna.upper().count('A')
+    t_count = dna.upper().count('T')
     at_content = (a_count + t_count) / length
-    return at_content
+    return round(at_content, 2)
 
 #we want out function to properly execute the following four lines
 my_at_content = get_at_content("ATGCGCGATCGATCGAATCG")
